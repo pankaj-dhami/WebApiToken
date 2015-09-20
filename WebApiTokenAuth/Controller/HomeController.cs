e47e7b1f7a700666840b05df49fad50584ccd4ed
pankaj <reachpankajdhami@gmail.com>
2015-09-20 22:36:13 +0530

    20 sept changes

--- WebApiTokenAuth/Controller/HomeController.cs
+++ WebApiTokenAuth/Controller/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,5 +16,29 @@ namespace WebApiTokenAuth.Controller
         {
             return View();
         }
-	}
+
+        public void setResumeDownload()
+        {
+
+            StorageCredentials credentials = new StorageCredentials("ultimateacc", "4F8JttoCMFvdrJ+ogx2mTZrU+xd1pBPwaa3O2CYTWFH3QEaWaUzc53ymj6N1q+0Xey4Ght6F9kQsRzb1FwIP2A==");
+            CloudStorageAccount account = new CloudStorageAccount(credentials, false);
+
+
+            // var account = CloudStorageAccount.FromConfigurationSetting("DataConnectionString");
+            var blobs = account.CreateCloudBlobClient();
+            var existingprops = blobs.GetServiceProperties();
+            blobs.SetServiceProperties(new Microsoft.WindowsAzure.Storage.Shared.Protocol.ServiceProperties
+            {
+                DefaultServiceVersion = "2011-08-18",
+                Logging = existingprops.Logging,
+                HourMetrics = existingprops.HourMetrics,
+                Metrics = existingprops.Metrics,
+                MinuteMetrics = existingprops.MinuteMetrics,
+                Cors = existingprops.Cors,
+
+
+            });
+        
+        }
+    }
 }
